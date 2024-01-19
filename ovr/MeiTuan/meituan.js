b12058f2832ea3735c210218b1c2f15764316cb2
mikoycc <54666324+mikoycc@users.noreply.github.com>
2024-01-19 22:13:55 +0800

    Update meituan.js

--- ovr/MeiTuan/meituan.js
+++ ovr/MeiTuan/meituan.js
@@ -1,4 +1,4 @@
-// For:https://github.com/chavyleung/scripts/blob/master/meituan/meituan.js
+//For: https://github.com/chavyleung/scripts/blob/master/meituan/meituan.js
 
 const cookieName = '美团'
 const tokenurlKey = 'chavy_tokenurl_meituan'
